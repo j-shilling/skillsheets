699f8b3928a2681c7a570461a9efa211766520f7
Jake Shilling <shilling.jake@gmail.com>
2018-01-27 15:19:22 -0500

    Serving dashboard.html instead of index.html

--- src/main/java/com/shilling/skillsheets/MainController.java
+++ src/main/java/com/shilling/skillsheets/MainController.java
@@ -26,7 +26,7 @@ public class MainController {
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String index() {
 		this.logger.traceEntry();
-		return "index.html";
+		return "dashboard.html";
 	}
 
 }
